ae394a6e03a82c1f79a6aa9c7f9d3730f1bb3b52
kylerflyer <Kyle.Losaria@city.ac.uk>
2024-10-25 14:44:52 +0100

    Modified task2 sql

--- HW 4/task1.sql
+++ HW 4/task1.sql
@@ -1,7 +1,6 @@
-DROP DATABASE IF EXISTS pet_database;
-CREATE DATABASE pet_database;
+
 USE pet_database;
-DROP TABLE IF EXISTS petPet, petEvent;
+
 
 
 CREATE TABLE petPet (
--- HW 4/task2.sql
+++ HW 4/task2.sql
@@ -10,10 +10,11 @@ VALUES ('Hammy', 'Diane', 'hamster', 'm','2010-10-30',null);
 INSERT INTO petevent
 VALUES ('Hammy', '2020-10-15', 'vet','antobiotics');
 
+UPDATE petevent set remark = "5 kittens 2 male 3 female" where eventtype="litter" and petname="Fluffy";
 
 INSERT INTO petevent
 VALUES ('Claws','1997-08-03', 'vet', 'Broken ribs');
 
 UPDATE petpet set death='2020-09-01' where petname="Puffball";
 
-DELETE FROM petpet where owner="Harold";
+DELETE FROM petpet where owner="Harold" and species="dog";
